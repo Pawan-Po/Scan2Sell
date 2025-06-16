f1979df5bc625f760d0f793a4ce17bce899ead96
Pawan kr <krpawan16.po@gmail.com>
2025-06-16 17:15:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/pos/pos-client.tsx
+++ src/components/pos/pos-client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import *ాలు React from 'react';
+import * as React from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
